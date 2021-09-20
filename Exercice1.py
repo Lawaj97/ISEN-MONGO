053b952e48f060c728b9eaabc45ee257338758d9
Lawaj <pierrick.dumas@isen.yncrea.fr>
2021-09-20 10:05:44 +0200

    update

--- Exercice1.py
+++ Exercice1.py
@@ -1,11 +1,28 @@
-from pymongo import MongoClient
+import pymongo
 import requests
 import json
+
 
 def get_vlille():
 
     url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion&refine.commune=LILLE"
     response = requests.requests("GET", url)
-    response_json = json.loads(response.text.encorede('utf8'))
+    response_json = json.loads(response.text.encode('utf8'))
     return response_json.get("records", [])
 
+#client = pymongo.MongoClient("mongodb+srv://lawaj:P*123456789p@isen-mongo-tp.1lwab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+db = client["ISEN-MONGO-TP"]
+
+col_stations = db["stations"]
+col_infostations = db["infostations"]
+
+"""
+["ville"]
+["geolocation"]
+["size"]
+["name"]
+["tpe"]
+["available"]
+"""
